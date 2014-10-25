4b7eaf373790573e9396ffe48b3ce06b0fd7f8b2
GabrielAppleby <gabrielappleby@ip-127-236.wireless.oberlin.edu>
2014-10-24 21:05:32 -0400

    outlined how i will do remaining tasks

--- Assets/Resources/Scripts/Gabe'sScrapPaper.js
+++ Assets/Resources/Scripts/Gabe'sScrapPaper.js
@@ -1,2 +1,24 @@
-﻿Vector3 dmgTextLocation = Camera.main.WorldToScreenPoint(transform.position);
-dmgTectLocation.x /= Screen.width;
+﻿//How Gabe would do damage text
+/*
+Vector3 dmgTextLocation = Camera.main.WorldToScreenPoint(transform.position);
+dmgTextLocation.x /= Screen.width;
+dmgTextLocation.y /= Screen.height;
+
+var damageText : GUITEXT;
+var damageObject = new GameObject("DamageText");
+damageObject.transform.position = dmgTextLocation;
+damageText = damageObject.addComponent(GUIText);
+damageText.text = VARIABLE WITH HOW MUCH DAMAGE THE SPELL/THING DOES
+damageText.fontSize = 20;
+Destroy(damageObject, 1);
+
+//How Gabe will make everything collide
+	//Add rigidbody2ds + box collider2ds to eveything
+	//Everythign should get fixed angle set equal to true
+	//Things that will not move should be kinematic
+
+
+//How Gabe will do knockback
+	//On trigger enter just apply force over time in the opposite direction?
+	
+*/
