722a0f28278b8f7f1db6cfdd302a9420cfed2736
Ash927 <connordavies11@gmail.com>
2014-11-24 23:05:21 -0500

    it's so pretty

--- Assets/Resources/Scripts/CharacterSelect.js
+++ Assets/Resources/Scripts/CharacterSelect.js
@@ -1,8 +1,22 @@
-﻿function Awake () {
+﻿var circle : Texture2D;
+var triangle : Texture2D;
+
+
+
+function Awake () {
+
+	circle = Resources.Load("Textures/circle",Texture2D);
+	triangle = Resources.Load("Textures/triangle",Texture2D);
+
+	var background = new GameObject().CreatePrimitive(PrimitiveType.Quad);
+	background.renderer.material.mainTexture = Resources.Load("Textures/shop",Texture2D); //Set the texture.  Must be in Resources folder
+	background.renderer.material.shader = Shader.Find ("Diffuse");	
+	background.transform.localScale = Vector3(13.8,10,1);					// Tell the renderer that our textures have transparency. 
+
 	var titleO = new GameObject("AnnounceText");
-	titleO.transform.position = Vector3(.1, .95, -1);
+	titleO.transform.position = Vector3(.3, .95, -1);
 	title = titleO.AddComponent(GUIText); 
-	title.text = "Use upper buttons to fight the boss";
+	title.text = "CHOOSE YOUR CLASS";
 	title.fontSize = Screen.height/19;
 	var levelLoader = new GameObject();
 	if (GameObject.Find("Level Loader")) {
@@ -16,24 +30,27 @@
 	}
 }
 
+/*
+function OnGUI(){
+	GUI.Box(Rect(Screen.width*0.25, Screen.height*0.25, Screen.width*0.5, Screen.height*0.6), "Main Menu");
+		if(GUI.Button (Rect (Screen.width*0.375, Screen.height*0.35, Screen.width*0.25, Screen.height*0.07), "Play")){
+			GameObject.Find("Level Loader").GetComponent(LevelLoaderScript).loadLevel("shop"); //Ideally this would go to a level select sreen.
+		}
+		if(GUI.Button (Rect (Screen.width*0.375, Screen.height*0.45, Screen.width*0.25, Screen.height*0.07), "Tutorial")){
+			GameObject.Find("Level Loader").GetComponent(LevelLoaderScript).loadLevel("tutorial1"); //Ideally this would go to a level select sreen.
+		}
+		if(GUI.Button (Rect (Screen.width*0.375, Screen.height*0.55, Screen.width*0.25, Screen.height*0.07), "Quit")){
+			Application.Quit();
+		}
+}
+
+*/
+
 function OnGUI() {
-	if(GUI.Button (Rect (Screen.width*0.2, Screen.height*0.45, Screen.width*0.1, Screen.height*0.1), "Warrior")) {
+	if(GUI.Button (Rect (Screen.width*0.1, Screen.height*0.3, Screen.width*0.3, Screen.height*0.4), circle)) {
 		GameObject.Find("Level Loader").GetComponent(LevelLoaderScript).loadNextLevel("Circle");
 	}
-	if(GUI.Button (Rect (Screen.width*0.45, Screen.height*0.45, Screen.width*0.1, Screen.height*0.1), "Archer")) {
+	if(GUI.Button (Rect (Screen.width*0.6, Screen.height*0.3, Screen.width*0.3, Screen.height*0.4), triangle)) {
 		GameObject.Find("Level Loader").GetComponent(LevelLoaderScript).loadNextLevel("Triangle");
-	}
-	if(GUI.Button (Rect (Screen.width*0.7, Screen.height*0.45, Screen.width*0.1, Screen.height*0.1), "Mage")) {
-		GameObject.Find("Level Loader").GetComponent(LevelLoaderScript).loadNextLevel("Square");
-	}
-	if(GUI.Button (Rect (Screen.width*0.2, Screen.height*0.25, Screen.width*0.1, Screen.height*0.1), "Warrior")) {
-		GameObject.Find("Level Loader").GetComponent(LevelLoaderScript).loadLevel("boss1","Circle");
-	}
-	if(GUI.Button (Rect (Screen.width*0.45, Screen.height*0.25, Screen.width*0.1, Screen.height*0.1), "Archer")) {
-		GameObject.Find("Level Loader").GetComponent(LevelLoaderScript).loadLevel("boss1","Triangle");
-	}
-	if(GUI.Button (Rect (Screen.width*0.7, Screen.height*0.25, Screen.width*0.1, Screen.height*0.1), "Mage")) {
-		GameObject.Find("Level Loader").GetComponent(LevelLoaderScript).loadLevel("boss1","Square");
-		
 	}
 }
