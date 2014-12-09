c10da7dd9f1c819ba8a96c66f21706ff986c6672
Ash927 <connordavies11@gmail.com>
2014-12-09 11:33:11 -0500

    COLORS!  EASTER!

--- Assets/Resources/Scripts/BuildWorldScript.js
+++ Assets/Resources/Scripts/BuildWorldScript.js
@@ -1,5 +1,6 @@
 ﻿//Holds characters corresponding to terrain
 private var world : Array; //Array to hold the world
+private var models : Array; //Array of the ground/rock models
 private var exampleMesh : Mesh;  //Mesh so we can not create primitive objects to hold things, before we switch to sprites
 private var rockParent : GameObject;		//Parent Object for the rocks
 private var groundParent : GameObject;		//Parent Object for the grounds
@@ -13,6 +14,7 @@ public static var levelEndLocation : Vector2;
 function proceduralInit(a : Array, exampleMesh : Mesh){
 	var plg : CreateLevel = gameObject.AddComponent(CreateLevel); //Something to do with random level generation?
 	world = a; //World array is the array passed in
+	models = new Array();
 	this.exampleMesh = exampleMesh; //assign the exampleMesh
 	
 	//Object parents for sanity
@@ -23,10 +25,15 @@ function proceduralInit(a : Array, exampleMesh : Mesh){
 	maxX = 60; //Max x value of map, explicitely set for now
 	maxY = 50; //Max y value of map, explicitely set for now
 	world.length=maxY;	//world length = maximum Y value
+	models.length=maxY;
+	for(var i = 0; i<models.length;i++){
+		models[i] = new Array();
+		models[i].length = maxX;
+	}
 	world = plg.createLevel(maxX,maxY, world); //Create the world
 	
 	//Build the world
-	for(var i = 0; i<maxY;i++){
+	for(i = 0; i<maxY;i++){
 		for(var j = 0; j<world[i].length;j++){
 			if(world[i][j]=="R"){
 				buildRock(j,i);
@@ -68,54 +75,84 @@ function proceduralInit(a : Array, exampleMesh : Mesh){
 		}
 	}
 	
+	seeds = new Array();
+	seeds.length = 50;
+	
+	for(i = 0;i<50;i++){
+		randX = Random.Range(0, maxX-1);
+		randY = Random.Range(0, maxY-1);
+		models[randY][randX].transform.GetChild(0).renderer.material.color = Color(1-(Random.value/2), 1-(Random.value/2), 1-(Random.value/2));
+		seeds[i] = [randY, randX];
+	}
+	
+	spreadColor(seeds);
+	
 	return world;
 
 	
-	/*
-	//WHHHHHY
-	var randX : int = Random.Range(0,maxX-1); //Random variable between 0 and max map X value
-	var randY : int = Random.Range(0,maxY-1); //Random variable between 0 and max map Y value
-	//While the randomly selected tile of the map is not just ground, reroll numbers
-	while(world[randY][randX]!= "G") {
-		randX = Random.Range(0,maxX-1); //Random variable between 0 and max map X value
-		randY = Random.Range(0,maxY-1); //Random variable between 0 and max map Y value
-	}
-	world[randY][randX] = world[randY][randX] + "L"; //Update the world array to show that there is also a levelend there
-	buildInteractables(hashNames["L"], randX,randY); //Builds the level end portal 
 	
-	randX = Random.Range(0,maxX-1); //Random variable between 0 and max map X value
-	randY = Random.Range(0,maxY-1); //Random variable between 0 and max map Y value
-	//While the randomly selected tile of the map is not just ground, reroll numbers
-	while(world[randY][randX]!= "G") {
-		randX = Random.Range(0,maxX-1); //Random variable between 0 and max map X value
-		randY = Random.Range(0,maxY-1); //Random variable between 0 and max map Y value
-	}
-	world[randY][randX] = world[randY][randX] + "K"; //Update the world array to show that there is also a levelend there
-	buildInteractables(hashNames["K"], randX,randY); //Builds the level end portal
-	
-	
-	// designate the player start location
-	randX = Random.Range(0,maxX-1); //Random variable between 0 and max map X value
-	randY = Random.Range(0,maxY-1); //Random variable between 0 and max map Y value
-	//While the randomly selected tile of the map is not just ground, reroll numbers
-	while(world[randY][randX]!= "G") {
-		randX = Random.Range(0,maxX-1); //Random variable between 0 and max map X value
-		randY = Random.Range(0,maxY-1); //Random variable between 0 and max map Y value
-	}
-	world[randY][randX] = "P"; //Update the world array to show that there is a player there
-	*/
 	
 }
 
+function spreadColor(seeds : Array){
+	for(i =0; i<seeds.length;i++){
+		for(j=-1;j<=1;j++){
+			for(k=-1;k<=1;k++){
+				if(models[myRoundY(seeds[i][0]+j)][myRoundX(seeds[i][1]+k)].transform.GetChild(0).renderer.material.color == Color(1, 1, 1)){
+					//WILL BE CHANGED
+					models[myRoundY(seeds[i][0]+j)][myRoundX(seeds[i][1]+k)].transform.GetChild(0).renderer.material.color = models[seeds[i][0]][seeds[i][1]].transform.GetChild(0).renderer.material.color;
+					seeds.Add([myRoundY(seeds[i][0]+j), myRoundX(seeds[i][1]+k)]);
+				}
+			}
+		}
+	}
+}
 
-//Start building the world
-/*function init(a : Array, exampleMesh : Mesh) {
-	world = a; //Set the world array to reference the array passed in
-	this.exampleMesh = exampleMesh; //exampleMesh for objects
-	rockParent = new GameObject("Rocks"); //parents for sanity
-	groundParent = new GameObject("Grounds"); //parents for sanity
-	buildWorld(); //Builds the world
+/*function spreadColor(){
+	counter=10;
+	//while(counter>0){
+	counter = 0;
+	print("TEST");
+	for(i = 0; i<models.length;i++){
+		for(j = 0; j<models[i].length;j++){
+			if(models[i][j].transform.GetChild(0).renderer.material.color != Color(1,1,1)){
+				for(k = -1; k<2; k++){
+					for(l = -1; l<2; l++){
+						if(models[myRoundY(i+k)][myRoundX(j+l)].transform.GetChild(0).renderer.material.color == Color(1,1,1)){
+							print("TEST" + i + ", " + j);
+							models[myRoundY(i+k)][myRoundX(j+l)].transform.GetChild(0).renderer.material.color = models[i][j].transform.GetChild(0).renderer.material.color;
+						}
+					}	
+				}
+			}
+			else{
+				counter++;
+			}
+		}
+	}
+//	}
 }*/
+
+function myRoundY(i : int){
+	if(i<0){
+		return 0;
+	}
+	if(i>49){
+		return 49;
+	}
+	return i;
+}
+
+function myRoundX(i : int){
+	if(i<0){
+		return 0;
+	}
+	if(i>59){
+		return 59;
+	}
+	return i;
+}
+
 
 //Builds a boss room
 function bossInit(a : Array, exampleMesh : Mesh, bossNum : int) {
@@ -129,92 +166,7 @@ function bossInit(a : Array, exampleMesh : Mesh, bossNum : int) {
 	}
 
 }
-/*
-//Builds the world
-function buildWorld() {
-	//var maxX = 60; //Max x value of map, explicitely set for now
-	var maxY = 50; //Max y value of map, explicitely set for now (never used atm)
-	var R : String = "R"; //Represents a tile of rock in the environment
-	var G : String = "G"; //Represents a tile of ground in the environment
-	var P : String = "P"; // Represents the player's starting location
-	world.length=maxY;	//world length = maximum Y value
-	world[0] =  [R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[1] =  [R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[2] =  [R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[3] =  [R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[4] =  [R, R, R, R, G, G, G, R, R, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[5] =  [R, R, R, R, G, G, G, R, R, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[6] =  [R, R, R, R, G, G, G, R, R, R, R, R, G, G, G, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[7] =  [R, G, G, G, G, G, G, G, G, R, R, R, G, G, G, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[8] =  [R, G, G, G, G, G, G, G, G, R, R, R, G, G, G, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[9] =  [R, G, G, G, G, G, G, G, G, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[10] = [R, G, G, G, G, G, G, G, G, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[11] = [R, G, G, G, G, G, G, G, G, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[12] = [R, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[13] = [R, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[14] = [R, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[15] = [R, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[16] = [R, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[17] = [R, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[18] = [R, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[19] = [R, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[20] = [R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[21] = [R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[22] = [R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[23] = [R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[24] = [R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[25] = [R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[26] = [R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[27] = [R, G, G, G, G, G, G, G, G, G, R, G, G, G, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[28] = [R, G, G, G, G, G, G, G, G, G, R, G, G, G, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[29] = [R, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[30] = [R, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[31] = [R, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[32] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[33] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[34] = [R, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[35] = [R, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[36] = [R, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[37] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[38] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[39] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	world[40] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R];
-	world[41] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R];
-	world[42] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, R];
-	world[43] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R];
-	world[44] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R];
-	world[45] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R];
-	world[46] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R];
-	world[47] = [R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R];
-	world[48] = [R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R];
-	world[49] = [R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, G, G, G, R, R, R, R, R, R, R, R, R, R];
-	world[50] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R];
-	world[51] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R];
-	world[52] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R];
-	world[53] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R];
-	world[54] = [R, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R];
-	world[55] = [R, G, G, G, P, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R];
-	world[56] = [R, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R];
-	world[57] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R];
-	world[58] = [R, G, G, G, G, G, G, G, G, G, R, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R, R, G, G, G, G, G, G, G, G, G, G, G, R, R, R, R];
-	world[59] = [R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R];
-	
-	//Uses the array representation of the world to spawn the actual objects
-	for(var i = 0; i<world.length;i++){
-		for(var j = 0; j<world[i].length;j++){
-			if(world[i][j]=="R"){
-				buildRock(j, world.length-i);
-			}
-			if(world[i][j]=="G"){
-				buildGround(j, world.length-i);
-			}
-			if (world[i][j] == "P") {
-				buildGround(j,world.length-i);
-			}
-		}
-	}
-	//buildLevelEnd(22, 48); //Builds the level end portal
-}*/
+
 
 function buildSpiderRoom() {
 	var maxY = 23; //Max y value of map, explicitely set for now (never used atm)
@@ -291,28 +243,8 @@ function buildRock(x : float, y : float){
 	
 	terrainScript.init(terrainType, exampleMesh); //Initialize the terrain script.
 	terrainScript.name = "ROCK"; //Give the terrain object a name in the Hierarchy pane.
-	
-	
-	
-	
-	/*var childCollider = new GameObject();// 3D colliders needed to raycast through walls
-	childCollider.transform.parent = terrainObject.transform;
-	childCollider.transform.localPosition = Vector3(0,0,0);
-	childCollider.AddComponent(BoxCollider); */
-	
-							//Turn off gravity
-	
-	//TESTTEST
-	
-	//var boxCollider2D = terrainObject.AddComponent(BoxCollider2D); //Add a box collider
-	//var rigidModel = terrainObject.AddComponent(Rigidbody2D); //Add a rigid body for collisions
-	//rigidModel.isKinematic = false;
-	//rigidModel.gravityScale = 0;
-	
-	//TESTTEST
-	
-	
-	
+
+	models[y][x] = terrainObject;
 }
 
 //Builds a ground object
@@ -325,6 +257,8 @@ function buildGround(x : float, y : float){
 	terrainScript.init(terrainType, exampleMesh); //Initialize the terrain script.
 	terrainScript.name = "Ground"; //Give the terrain object a name in the Hierarchy pane.
 	terrainScript.transform.parent = groundParent.transform; //Gives it a parent in hierarchy pane
+	
+	models[y][x] = terrainObject;
 }
 
 /*
